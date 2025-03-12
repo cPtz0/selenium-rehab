f952abd18e812d894b04cf640b098d53ede47e5f
cPtz0 <712frank@gmail.com>
2025-03-12 13:28:18 +0800

    feature: 製做錯誤改動模擬

--- login.py
+++ login.py
@@ -1,9 +1,6 @@
-print("debug")
-print("Main user login:")
-print("Is a not to use")
-print('111111')
+print("debug.....")
+print("Here,Main user login:")
 print("login...")
-
 username = input("輸入帳號:")
 pwd = input("輸入密碼:")
 print("登入成功")
