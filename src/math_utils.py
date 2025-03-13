2c83981f1b6c6a092ecb13005cabe9d5f5cef878
cPtz0 <712frank@gmail.com>
2025-03-13 16:27:35 +0800

    feat: 新增部分數學函式 add/subtract/devide/sqrt

--- src/math_utils.py
+++ src/math_utils.py
@@ -2,6 +2,7 @@
 # x : float 表示預期 x 為浮點數
 # -> float 表示回傳值為浮點數
 # 使用三引號(PEP257規範)
+import math
 def square(x : float) -> float:
     """
     計算正方形面積的函式
@@ -22,11 +23,114 @@ def square(x : float) -> float:
         >>> square(5)
         25.0
     """
+    if not isinstance(x,(int,float)):
+        raise ValueError("請輸入數值!")
     return x * x
+def add(x : float , y : float) -> float:
+    """
+    計算兩數相加之和
+
+    Args:
+        x(float): 傳入第一個參數,用於相加。
+        y(float): 傳入第二個參數,用於相加。
+ 
+    Returns:
+        float: x + y 的和
+ 
+    Raises:
+        ValueError: 當輸入為非數值型態則會觸發此錯誤。
+ 
+    Example:
+        >>> add(1,2)
+        3
+    """
+    if not isinstance(x,(int,float)):
+        raise ValueError("請輸入數值!")
+    if not isinstance(y,(int,float)):
+        raise ValueError("請輸入數值!")
+    return x + y
+
+def subtract(x : float , y : float) -> float:
+    """
+    計算兩數相減之解
+
+    Args:
+        x(float): 傳入第一個參數,用於相減。
+        y(float): 傳入第二個參數,用於相減。
+ 
+    Returns:
+        float: x - y 的解
+ 
+    Raises:
+        ValueError: 當輸入為非數值型態則會觸發此錯誤。
+ 
+    Example:
+        >>> subtract(3,2)
+        1
+    """
+    if not isinstance(x,(int,float)):
+        raise ValueError("請輸入數值!")
+    return x - y
+
+def divide(x : float , y : float) -> float:
+    """
+    計算兩數相除之解
+
+    Args:
+        x(float): 傳入第一個參數,作為被除數。
+        y(float): 傳入第二個參數,作為除數。
+ 
+    Returns:
+        float: x / y 的解
+ 
+    Raises:
+        ValueError: 當輸入為非數值型態則會觸發此錯誤
+        ZeroDivisonError: 當除數為0,即 y = 0 時
+
+    Example:
+        >>> devide(4,2)
+        2
+    """
+    if not isinstance(x,(int,float)):
+        raise ValueError("請輸入數值!")
+    if y == 0:
+        raise ZeroDivisionError("除數不可為0")
+    
+    return x / y
+
+def sqrt(x : float) -> float:
+    """
+    計算一數之平方根
+
+    Args:
+        x(float): 傳入第一個參數,作為被除數。
+        y(float): 傳入第二個參數,作為除數。
+ 
+    Returns:
+        float: x / y 的解
+ 
+    Raises:
+        ValueError: 當輸入為非數值型態或 x < 0則會觸發此錯誤
+
+    Example:
+        >>> sqrt(9)
+        3
+    """
+    if not isinstance(x,(int,float)):
+        raise ValueError("請輸入數值!")
+    if x < 0:
+        raise ValueError("無法開根號小於0之數")
+    
+    return  math.sqrt(x) 
+
 
 if __name__ == "__main__":
     try:
         num = float(input("輸入一個數字:")) 
-        print(f"{num}的平方為:{square(5)}")
-    except ValueError:
-        print("無效輸入!")
+        num1 = float(input("輸入一個數字:")) 
+        print(f"{num1} + {num} 為:{add(num,num1)}")
+    except ValueError as ve: 
+        print(f"輸入錯誤{ve}")
+    except Exception as e:
+        print(f"預期外錯誤{e}")
+    
