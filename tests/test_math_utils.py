074f2e5efabf751aeac75d4c5ddbd1e4ce0cd6e2
cPtz0 <712frank@gmail.com>
2025-03-17 16:13:27 +0800

    test: 重構math_utils，加入分類

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,34 +1,66 @@
 import unittest
 from src.math_utils import add,subtract,divide,sqrt,square,factorial
 
-class TestMathUtils(unittest.TestCase):
-    # 正常數據測試
-    def test_math_add(self):
+#相加類測試
+class TestAdd(unittest.TestCase):
+    def test_math_add_valid(self):
         self.assertEqual(add(1,2),3)
-    def test_mat_subtract(self):
+    def test_math_add_invalid_input_type(self):
+        try:
+            add(1,'E')
+            self.fail("觸發錯誤ValueError,但是沒有發生")
+        except ValueError as e:
+            self.assertIn("數字",str(e))
+#相減類測試
+class TestSubtract(unittest.TestCase):
+    def test_math_subtract_valid(self):
         self.assertEqual(subtract(3,2),1)
-    def test_math_divide(self):
+    def test_math_subtract_invalid_input_type(self):
+        try:
+            add('E',2)
+            self.fail("觸發錯誤ValueError,但是沒有發生")
+        except ValueError as e:
+            self.assertIn("數字",str(e))
+#相除類測試
+class TestDivide(unittest.TestCase):
+    def test_math_divide_valid(self):
         self.assertEqual(divide(32,8),4)
-    def test_math_sqrt(self):
-        self.assertEqual(sqrt(49),7)
-    def test_math_square(self):
-        self.assertEqual(square(9),81)
-    def test_math_factorial(self): 
-        self.assertEqual(factorial(5),120)
-    # 異常數據測試
-    def test_math_devide_by_zero(self): 
+    def test_math_divide_invalid_input_zero(self): 
         with self.assertRaises(ZeroDivisionError):
             divide(100,0)
-    def test_math_sqrt_less_zero(self): 
+    def test_math_divide_invalid_input_type(self): 
+        try:
+            divide('100',5)
+            self.fail("觸發錯誤ValueError,但是沒有發生")
+        except ValueError as e:
+            self.assertIn("數字",str(e))
+#平方類計算
+class TestSquare(unittest.TestCase):
+    def test_math_square_valid(self):
+        self.assertEqual(square(9),81)
+    def test_math_divide_invalid_input_type(self): 
         with self.assertRaises(ValueError):
-            sqrt(-1)
-    def test_math_square_input_wrong_value(self): 
-        with self.assertRaises(ValueError):
-            square('Word')
-    def test_math_factorial_input_wrong_value(self): 
+            square('E')
+#階乘類計算
+class TestFactorial(unittest.TestCase):
+    def test_math_factorial_valid(self): 
+        self.assertEqual(factorial(5),120)
+    def test_math_factorial_invalid_input_negative(self): 
         with self.assertRaises(ValueError):
             factorial(-3)
-    
+    def test_math_factorial_invalid_input_type(self): 
+        with self.assertRaises(ValueError):
+            factorial('E')
+#開根號類測試
+class TestSqrt(unittest.TestCase):
+    def test_math_sqrt(self):
+        self.assertEqual(sqrt(49),7)
+    def test_math_sqrt_invalid_input_negative(self): 
+        with self.assertRaises(ValueError):
+            sqrt(-1)
+    def test_math_sqrt_invalid_input_type(self):
+        with self.assertRaises(ValueError):
+            sqrt('E')
 
 if __name__ == "main":
     unittest.main()
