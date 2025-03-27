decc658d5b5bc6f23c2636cf6870e48b6fc9bee5
cPtz0 <712frank@gmail.com>
2025-03-27 11:44:26 +0800

    test: 重構api/divide.py的邏輯問題；修正raiseError在不同測試中的狀態；新增test_api_divide_route

--- tests/test_api_divide.py
+++ tests/test_api_divide.py
@@ -1,19 +1,22 @@
 import unittest
-from api.divide import apiDivide
+import asyncio
+import inspect
+from api.divide import divide_core
+print("[DEBUG] divide core from :" ,inspect.getfile(divide_core))
 
 class TestApiDivide(unittest.TestCase):
     def test_api_divide_valid(self):
-        res = apiDivide(10,2)
-        print(res)
+        res = asyncio.run(divide_core(10,2))
+        self.assertEqual(res["result"],5)
     #
     def test_api_divide_by_zero(self):
+        tt = ''
         with self.assertRaises(ValueError) as context:
-
-            apiDivide(10,0)
-        self.assertIn("除數必須為",str(context.exception))
+            asyncio.run(divide_core(10,0))
+        self.assertIn("不能為0",str(context.exception)) #
     #
     def test_api_divide_by_word(self):
+        aa = ''
         with self.assertRaises(ValueError) as context:
-
-            apiDivide(10,'E')
-        self.assertIn("除數必須為",str(context.exception))
+            asyncio.run(divide_core(10,'E'))
+        self.assertIn("須為實數",str(context.exception)) #
